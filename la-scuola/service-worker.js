c814446da9c33b3375ff0d88d7af7d400a143893
Alberto Cantalù <albertocantalu@MacBook-Pro-di-Alberto.local>
2026-08-14 15:43:38 +0200

    Mostra il costume sul personaggio

--- la-scuola/service-worker.js
+++ la-scuola/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='the-school-v14';
+const CACHE='the-school-v15';
 const FILES=['./','index.html','manifest.webmanifest','icons/apple-touch-icon.png','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
